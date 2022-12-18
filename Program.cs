edbddc99cb8ffbe746bacc63db1119e199f03aff
singhtushar689 <singhtushar689@gmail.com>
2022-12-18 13:50:50 +0530

    Hello World

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicCorePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! Welcome");
+        }
+    }
+}
