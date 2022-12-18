2c2cc07c4525dd7a2305bedf334e1c1a61d3a880
singhtushar689 <singhtushar689@gmail.com>
2022-12-18 15:08:38 +0530

    Quotient And Remainder

--- Program.cs
+++ Program.cs
@@ -4,17 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Nunber");
-            int number = Convert.ToInt32(Console.ReadLine());
-            int factor = 0;
-            for(int i = 2; i < number/2; i++)
-            {
-                if(number % i == 0)
-                {
-                    Console.WriteLine("Number is divisible by {0} ",i);
-                }
-            }
-        }
-        
+            Console.WriteLine("Enter the Dividend");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+            Console.WriteLine("Quotient for the Given Number is {0}",quotient);
+            Console.WriteLine("Remainder for the Given Number is {0}",remainder);   
+        }   
     }
 }
